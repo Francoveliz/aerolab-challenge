cb4196099421257cb14795c253dd07d2c2da8ec4
Francoveliz <francoleandroveliz@gmail.com>
2021-06-05 18:45:22 -0300

    fix margin

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,11 @@ import headerImg from "../../assets/images/header-x1.png";
 const Header = () => {
 	return (
 		<div className="relative">
-			<img className="w-full z-0" src={headerImg} alt="auriculares" />
+			<img
+				className="w-full max-w-7xl mx-auto z-0"
+				src={headerImg}
+				alt="auriculares"
+			/>
 			<div className="container">
 				<h1 className="z-50 absolute bottom-0 mb-10 text-5xl text-white font-bold">
 					Electronics
